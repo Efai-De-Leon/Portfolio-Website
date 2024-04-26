1d2fe4a2da992e6476d52b38e8a29530af5b5de7
Efaideleon <efaideleon@gmail.com>
2024-04-25 19:01:38 -0700

    clean

--- src/components/mouse_position/IMousePosition.tsx
+++
@@ -1,9 +0,0 @@
-export interface IMousePosition {
-    _x: number;
-    _y: number;
-    x: number;
-    y: number;
-    updatePosition: (event: MouseEvent) => void;
-    setOrigin: (element: HTMLElement) => void;
-}
-
--- src/components/project_card_gallery/CardGallery.tsx
+++
@@ -1,8 +0,0 @@
-import { Card, CardProps } from '../project_card/Card'
-
-export type CardGalleryProps = {
-  cardsData: CardProps[]
-  Card: Card
-}
-
-export type CardGallery = (props: CardGalleryProps) => JSX.Element
