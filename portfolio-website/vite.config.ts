b0f5697e81c4b7f454728dd97d02e12529aee0a3
Efaideleon <efaideleon@gmail.com>
2024-04-25 19:18:11 -0700

    deploy

--- portfolio-website/vite.config.ts
+++ portfolio-website/vite.config.ts
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  assetsInclude: ['**/*.png', '**/*.svg', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp', `**/*.woff`, `**/*.woff2`, `**/*.eot`, `**/*.ttf`, `**/*.otf`]
 })
