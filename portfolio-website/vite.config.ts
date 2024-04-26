bf0fe1d3719bc3aa8628adc637d8fa21bc198292
Efaideleon <efaideleon@gmail.com>
2024-04-25 19:39:20 -0700

    react

--- portfolio-website/vite.config.ts
+++ portfolio-website/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.svg', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp', `**/*.woff`, `**/*.woff2`, `**/*.eot`, `**/*.ttf`, `**/*.otf`]
+  assetsInclude: ['**/*.png', '**/*.svg', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp', `**/*.woff`, `**/*.woff2`, `**/*.eot`, `**/*.ttf`, `**/*.otf`],
+  base: "/portfolio-website/",
 })
