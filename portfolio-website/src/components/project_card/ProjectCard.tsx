87b3112af91b58ce66e04f16fbba9ddd79046467
Efaideleon <efaideleon@gmail.com>
2024-04-26 14:04:33 -0700

    react

--- portfolio-website/src/components/project_card/ProjectCard.tsx
+++ portfolio-website/src/components/project_card/ProjectCard.tsx
@@ -3,28 +3,31 @@ import { Card, CardProps } from './Card'
 import { useRef } from 'react'
 import { useCircleBlurEffect } from '../blur_effect/useCircleBlurEffect'
 
-export const ProjectCard: Card = ({title, description, image, link}: CardProps) => {
+export const ProjectCard: Card = ({ title, description, image, link }: CardProps) => {
     const containerRef = useRef<HTMLDivElement>(null)
     const innerRef = useRef<HTMLDivElement>(null)
 
-    useCircleBlurEffect({containerRef: containerRef, innerRef: innerRef});
+    useCircleBlurEffect({ containerRef: containerRef, innerRef: innerRef });
     return (
-        <div ref={containerRef} className="container">
-            <article ref={innerRef} className="project-card small-card inner">
-                <div className="project-content">
-                    <h3>{title}</h3>
-                    <div className="project-overview">
-                        <p> {description} </p>
+        <a href={link} className="project-card-link">
+            <div ref={containerRef} className="container">
+                <article ref={innerRef} className="project-card small-card inner">
+                    <div className="project-content">
+                        <h3>{title}</h3>
+                        <div className="project-overview">
+                            <p> {description} </p>
+                        </div>
+                        <div className="project-info">
+                            <a href={link}
+                                className="btn">Github Repository</a>
+                        </div>
                     </div>
-                    <div className="project-info">
-                        <a href={link}
-                            className="btn">Github Repository</a>
-                    </div>
-                </div>
-                <figure>
-                    <img src={"./assets/" + image} alt="Not Found" />
-                </figure>
-            </article>
-        </div>
+
+                    <figure>
+                        <img src={"./assets/" + image} alt="Not Found" />
+                    </figure>
+                </article>
+            </div>
+        </a>
     )
 }
